9424bece39250e3642ee2bb5deaaf3a1d8911ebb
MATHMAN7 <159688574+MATHMAN7@users.noreply.github.com>
2025-08-11 09:25:18 +0200

    Initial commit: personal website project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Web_pages from './web_pages'
+
+function App() {
+
+
+  return (
+    <div className="App">
+        <Web_pages  />
+    </div>
+  )
+}
+
+export default App
---
+++ src/web_pages.jsx
@@ -0,0 +1,13 @@
+function Web_pages(){
+    return(
+        <div className="panel">
+
+            <button>Home</button>
+            <button>About Me</button>
+            <button>Projects</button>
+            <button>Contact Me</button>
+
+        </div>
+    )
+}
+export default Web_pages;
