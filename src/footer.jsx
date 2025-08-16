4aebba2cfe6753f0c6bbd4f4427499eef8fe89f7
MATHMAN7 <159688574+MATHMAN7@users.noreply.github.com>
2025-08-16 21:13:46 +0200

    Update footer section with improvements

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import './index.css';
+import './footer.css';
+import { Github, Linkedin, Instagram, Mail } from "lucide-react";
 
 function Footer() {
     const year = new Date().getFullYear();
     return (
+        <div  className="footerBox">
+
         <footer className="footer">
             © {year} Loren Smart. All rights reserved.
         </footer>
+            <a
+                href="https://instagram.com/thegreatmathman"
+                target="_blank"
+                rel="noopener noreferrer">
+
+                <Instagram size={20} />
+            </a>
+
+            <a href="mailto:sonycena844@gmail.com"
+
+
+            >
+                <Mail size={20} />
+            </a>
+
+
+        </div>
     );
 }
 export default Footer;
